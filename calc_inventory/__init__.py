6dc89fdf0af414dea2be6d6b058f3b44778131f9
Marc Engle <marcengle30@gmail.com>
2023-08-21 23:20:01 -0400

    adjusted classes

--- calc_inventory/__init__.py
+++ calc_inventory/__init__.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request, redirect
-from .classes import addition, subtraction, multiplication, division
+from .classes import Calculator
 
 app = Flask(__name__)
 
@@ -14,18 +14,15 @@ def calculate():
             form = request.form
             num1 = float(form['num1'])
             num2 = float(form['num2'])
+            arithmetic = Calculator(num1, num2)
             if form["submit_button"] == "add":
-                add_nums = addition(num1, num2)
-                return render_template('base.html', display=add_nums.add_the_numbers())
+                return render_template('base.html', display=arithmetic.add_the_numbers())
             elif form["submit_button"] == "subtract":
-                subtract_nums = subtraction(num1, num2)
-                return render_template('base.html', display=subtract_nums.subtract_the_numbers())
+                return render_template('base.html', display=arithmetic.subtract_the_numbers())
             elif form["submit_button"] == "multiply":
-                multiply_nums = multiplication(num1, num2)
-                return render_template('base.html', display=multiply_nums.multiply_the_numbers())
+                return render_template('base.html', display=arithmetic.multiply_the_numbers())
             elif form["submit_button"] == "divide":
-                divide_nums = division(num1, num2)
-                return render_template('base.html', display=divide_nums.divide_the_numbers())
+                return render_template('base.html', display=arithmetic.divide_the_numbers())
         except:
             return render_template('base.html', display="Enter Valid Numbers and Try Again")
         
--- calc_inventory/classes.py
+++ calc_inventory/classes.py
@@ -1,34 +1,19 @@
-class addition():
+class Calculator():
     def __init__(self, numOne, numTwo):
         self.num1 = numOne
         self.num2 = numTwo
 
     def add_the_numbers(self):
         return self.num1 + self.num2
-    
-
-class subtraction():
-    def __init__(self, numOne, numTwo):
-        self.num1 = numOne
-        self.num2 = numTwo
 
     def subtract_the_numbers(self):
         return self.num1 - self.num2
-    
-
-class multiplication():
-    def __init__(self, numOne, numTwo):
-        self.num1 = numOne
-        self.num2 = numTwo
 
     def multiply_the_numbers(self):
         return self.num1 * self.num2
-    
-
-class division():
-    def __init__(self, numOne, numTwo):
-        self.num1 = numOne
-        self.num2 = numTwo
 
     def divide_the_numbers(self):
-        return self.num1 / self.num2
+        if self.num2 != 0:
+            return self.num1 / self.num2
+        else:
+            raise ValueError("Division by zero is not allowed.")
